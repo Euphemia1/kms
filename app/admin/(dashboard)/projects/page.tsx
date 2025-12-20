47e301de0e31e803ce8f730e0e30f200c7d5b6ed
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-12-20 13:34:26 +0200

    add chnages to ocdebase

--- app/admin/(dashboard)/projects/page.tsx
+++ app/admin/(dashboard)/projects/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { query } from "@/lib/db"
 import { Button } from "@/components/ui/button"
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from "@/components/ui/card"
