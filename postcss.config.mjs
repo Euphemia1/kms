939c2ef3dcf02e81a98a266d2c72f64cdef316aa
Euphemia1 <euphemiachikungulu347@gmail.com>
2025-12-29 18:34:04 +0200

    add changes to codebase

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,11 +1,8 @@
-import tailwindcss from '@tailwindcss/postcss'
-import autoprefixer from 'autoprefixer'
-
 /** @type {import('postcss').PostConfig} */
 const config = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    '@tailwindcss/postcss': {},
+    'autoprefixer': {},
   },
 }
 
